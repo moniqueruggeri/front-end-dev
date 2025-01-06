560637f599dba2218d6781f2114e64f2b0577b4f
Monique Ruggeri <ruggeri.monique@gmail.com>
2025-01-06 09:09:55 +0000

    home pronta

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,14 +1,27 @@
 import Button from "../components/Button";
 import Icons from "../components/Icons";
 import "../sass/layout/home.sass";
+import monique from "../img/monique.png";
 
 const Home = () => {
   return (
     <main className="home">
       <Icons />
-      <h1>
-        front <br></br>end dev
-      </h1>
+      <div className="titleHome">
+        <h2 className="stroke stroke-left">
+          front <br />
+          end dev
+        </h2>
+        <h1>
+          front <br />
+          end dev
+        </h1>
+        <h2 className="stroke stroke-right">
+          front <br />
+          end dev
+        </h2>
+        <img src={monique} alt="" />
+      </div>
       <Button
         action={"expand"}
         text={"Explore"}
